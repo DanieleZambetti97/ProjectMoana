826bba1f89c351f8eb1ff76ba3cf13add253735c
Giacomo Rivolta <giacomo.rivolta@studenti.unimi.it>
2021-03-10 16:53:12 +0100

    Formatting

--- Raytracer/src/Raytracer.jl
+++ Raytracer/src/Raytracer.jl
@@ -3,11 +3,14 @@ module Raytracer
 import ColorTypes
 import Base.:+, Base.:*, Base.:≈
 
+
 greet(name) = println("Hello $(name)! Moana welcomes you!")
+
 
 # Sum of two colors 
 
 Base.:+(c1::ColorTypes.RGB{T}, c2::ColorTypes.RGB{T}) where {T} = ColorTypes.RGB((c1.r + c2.r), (c1.g + c2.g), (c1.b + c2.b))
+
 
 
 # Difference of two colors 
@@ -15,9 +18,11 @@ Base.:+(c1::ColorTypes.RGB{T}, c2::ColorTypes.RGB{T}) where {T} = ColorTypes.RGB
 Base.:-(c1::ColorTypes.RGB{T}, c2::ColorTypes.RGB{T}) where {T} = ColorTypes.RGB((c1.r - c2.r), (c1.g - c2.g), (c1.b - c2.b))
 
 
+
 # Product "scalar * color"
 
 Base.:*(c::ColorTypes.RGB{T}, scalar) where {T} = ColorTypes.RGB(c.r*scalar, c.g*scalar, c.b*scalar)
+
 
 
 # Product "color * scalar"
@@ -25,9 +30,10 @@ Base.:*(c::ColorTypes.RGB{T}, scalar) where {T} = ColorTypes.RGB(c.r*scalar, c.g
 Base.:*(scalar, c::ColorTypes.RGB{T}) where {T} = ColorTypes.RGB(c.r*scalar, c.g*scalar, c.b*scalar)
 
 
+
 # Aprroxmation for two color
 
 Base.:isapprox(c1::ColorTypes.RGB{T}, c2::ColorTypes.RGB{T}) where {T} = Base.isapprox(c1.r,c2.r) && Base.isapprox(c1.g,c2.g) && Base.isapprox(c1.b,c2.b) 
-# volendo si può copiare il carattere ≈ dalla linea 4 che ha scritto Tomasi, ma da tastiera io non riesco a mettercelo in questo file
+
 
 end # module
