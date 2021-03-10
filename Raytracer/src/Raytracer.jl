7dd2ca07d3be190f3c1dbafda930d2ac55772822
Giacomo Rivolta <giacomo.rivolta@studenti.unimi.it>
2021-03-10 11:45:32 +0100

    Add method for colors sum

--- Raytracer/src/Raytracer.jl
+++ Raytracer/src/Raytracer.jl
@@ -1,5 +1,12 @@
 module Raytracer
 
-greet() = print("Hello World!")
+import ColorTypes
+import Base.:+, Base.:*, Base.:≈
+
+greet(name) = print("Hello $(name)! Moana welcomes you!")
+
+#modifing the sum of two colors 
+
+Base.:+(c1::ColorTypes.RGB{T}, c2::ColorTypes.RGB{T}) where {T} = ColorTypes.RGB((c1.r + c2.r), (c1.g + c2.g), (c1.b + c2.b))
 
 end # module
