911aece16ab7281d7563e3fc2c29539c032c0518
Daniele Zambetti <zambettidaniele97@gmail.com>
2021-03-10 11:44:02 +0100

    Add method for scalar product

--- Raytracer/src/Raytracer.jl
+++ Raytracer/src/Raytracer.jl
@@ -1,5 +1,22 @@
 module Raytracer
 
 greet() = print("Hello World!")
+import ColorTypes
+
+import Base.:*, Base.:+ 
+
+# Definition of product "scalar * color"
+Base.:*(c::ColorTypes.RGB{T}, scalar) where {T} = ColorTypes.RGB(c.r*scalar, c.g*scalar, c.b*scalar)
+
+
+
+
+
+
+
+
+
+#Definition of sum "color + color"
+Base.:+(c1::ColorTypes.RGB{T}, c2::ColorTypes.RGB{T}) where {T} = c1 + c2
 
 end # module
