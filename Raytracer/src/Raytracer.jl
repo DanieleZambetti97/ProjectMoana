431d6f07c81c4717b684920141d3db3cc29deaed
Giacomo Rivolta <giacomo.rivolta@studenti.unimi.it>
2021-03-10 11:50:49 +0100

    Add method for color difference

--- Raytracer/src/Raytracer.jl
+++ Raytracer/src/Raytracer.jl
@@ -5,11 +5,15 @@ import Base.:+, Base.:*, Base.:≈
 
 greet(name) = print("Hello $(name)! Moana welcomes you!")
 
-# modifing the sum of two colors 
+# Definition of the sum of two colors 
 
 Base.:+(c1::ColorTypes.RGB{T}, c2::ColorTypes.RGB{T}) where {T} = ColorTypes.RGB((c1.r + c2.r), (c1.g + c2.g), (c1.b + c2.b))
 
-# Definition of product "scalar * color"
+# Definition of the difference of two colors 
+
+Base.:-(c1::ColorTypes.RGB{T}, c2::ColorTypes.RGB{T}) where {T} = ColorTypes.RGB((c1.r - c2.r), (c1.g - c2.g), (c1.b - c2.b))
+
+# Definition of the product "scalar * color"
 
 Base.:*(c::ColorTypes.RGB{T}, scalar) where {T} = ColorTypes.RGB(c.r*scalar, c.g*scalar, c.b*scalar)
 
