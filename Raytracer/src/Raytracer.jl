59f06d84cebf284c66796fe32e9e5d02e45cbf8c
Daniele Zambetti <zambettidaniele97@gmail.com>
2021-03-10 11:52:21 +0100

    Method for the commutative scalar product of color and number

--- Raytracer/src/Raytracer.jl
+++ Raytracer/src/Raytracer.jl
@@ -10,7 +10,9 @@ greet(name) = print("Hello $(name)! Moana welcomes you!")
 Base.:+(c1::ColorTypes.RGB{T}, c2::ColorTypes.RGB{T}) where {T} = ColorTypes.RGB((c1.r + c2.r), (c1.g + c2.g), (c1.b + c2.b))
 
 # Definition of product "scalar * color"
-
 Base.:*(c::ColorTypes.RGB{T}, scalar) where {T} = ColorTypes.RGB(c.r*scalar, c.g*scalar, c.b*scalar)
+
+# Definition of product "color * scalar"
+Base.:*(scalar, c::ColorTypes.RGB{T}) where {T} = ColorTypes.RGB(c.r*scalar, c.g*scalar, c.b*scalar)
 
 end # module
