f5fdecd44b4f2baf27d6d9a611843e07fae4b97e
Giacomo Rivolta <giacomo.rivolta@studenti.unimi.it>
2021-03-17 11:41:09 +0100

    updating methods

--- HdrImage/src/HdrImage.jl
+++ HdrImage/src/HdrImage.jl
@@ -1,13 +1,17 @@
 module HdrImage
-using Colors
-using Raytracer
+import Colors
+import ColorTypes
+import Base
 
 greet() = print("Hello World!")
 
-struct HdrImage 
+mutable struct HdrImages
     heigth::Int
     width::Int
-    pixels = [Color() for i in range(heigth*width)]
+    pixels::Array{ColorTypes.RGB, 1}
+    HdrImages(w, h) = new(w, h, [ColorTypes.RGB() for i in 1:h*w])
 end
+
+#img = HdrImage(10, 5, pixel)
 
 end # module
