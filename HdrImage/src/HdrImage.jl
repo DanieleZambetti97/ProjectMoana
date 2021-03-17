499690f6a164d0961027be69162d1bca9c5c2f83
Daniele Zambetti <zambettidaniele97@gmail.com>
2021-03-17 11:03:33 +0100

    General structure of HdrImage

--- HdrImage/src/HdrImage.jl
+++ HdrImage/src/HdrImage.jl
@@ -1,13 +1,17 @@
 module HdrImage
-using Colors
-using Raytracer
+import Colors
+import ColorTypes
 
 greet() = print("Hello World!")
 
+#struct that create a matrix heigth*width in which every element is a RGB color
 struct HdrImage 
-    heigth::Int
-    width::Int
-    pixels = [Color() for i in range(heigth*width)]
+    heigth
+    width
+    pixels
 end
+
+#method for save a HdrImage on a file in PFM format
+write_pfm()
 
 end # module
