df8deec685dfba18a06e6d948f8e341a8d9f3962
Daniele Zambetti <zambettidaniele97@gmail.com>
2021-03-31 10:55:39 +0200

    average_luminosity test

--- src/HdrImages.jl
+++ src/HdrImages.jl
@@ -158,9 +158,14 @@ end
 ################################################################################################################
 # Save an LdrImage on an output file
 
-function average_luminosity(img::HdrImage, delta=1^-10)
+function average_luminosity(img::HdrImage, delta)
     sum = 0.0
     for pixel in img.pixels
         sum += log10(delta + luminosity(pixel))
+    end
     return 10^(sum/length(img.pixels))
+end
+
+function average_luminosity(img::HdrImage)
+    return average_luminosity(img, 10^(-10))
 end
