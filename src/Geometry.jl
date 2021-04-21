bb882d081591282d6ee810d5c79c4629935d3b23
Giacomo Rivolta <giacomo.rivolta@studenti.unimi.it>
2021-04-21 07:29:40 +0200

    Modifying Geometry.jl

--- src/Geometry.jl
+++ src/Geometry.jl
@@ -1,12 +1,14 @@
+import Base.:+, Base.:*, Base.:≈, Base.:-
+
 export Vec, Point, cross, squared_norm, norm, normalize
 
-# Implementation of new type Vec
 struct Vec
-    x::Float64
-    y::Float64
-    z::Float64
+    vx::Float64
+    vy::Float64
+    vz::Float64
 
 end
+
 
 # Implementation of new type Point
 struct Point
@@ -15,43 +17,42 @@ struct Point
     z::Float64
 end
 
-# Basic operations for Vec
-Base.:isapprox(V1::Vec, V2::Vec) = Base.isapprox(V1.x, V2.x) && Base.isapprox(V1.y, V2.y) && Base.isapprox(V1.z, V2.z)
+Base.:isapprox(V1::Vec, V2::Vec) = Base.isapprox(V1.vx,V2.vx) && Base.isapprox(V1.vy,V2.vy) && Base.isapprox(V1.vz,V2.vz)
 
-Base.:+(V1::Vec , V2::Vec ) = Vec( (V1.x+V2.x), (V1.y+V2.y), (V1.z+V2.z) )
+Base.:+(V1::Vec , V2::Vec )  = Vec((V1.vx+V2.vx), (V1.vy+V2.vy), (V1.vz+V2.vz))
 
-Base.:-(V1::Vec , V2::Vec ) = Vec( (V1.x-V2.x), (V1.y-V2.y), (V1.z-V2.z) )
+Base.:-(V1::Vec , V2::Vec )   = Vec((V1.vx-V2.vx), (V1.vy-V2.vy), (V1.vz-V2.vz))
 
-Base.:*(V1::Vec , scalar::Real) = Vec(V1.x*scalar, V1.y*scalar, V1.z*scalar)
+Base.:*(V1::Vec , scalar::Real)   = Vec(V1.vx*scalar, V1.vy*scalar, V1.vz*scalar)
 
-Base.:*(scalar::Real, V1::Vec ) = Vec(V1.x*scalar, V1.y*scalar, V1.z*scalar)
+Base.:*(scalar::Real, V1::Vec )   = Vec(V1.vx*scalar, V1.vy*scalar, V1.vz*scalar)
 
-# Dot and cross product
-Base.:*(V1::Vec , V2::Vec ) = (V1.x*V2.x)+(V1.y*V2.y)+(V1.z*V2.z)
+Base.:*(V1::Vec , V2::Vec )   = (V1.vx*V2.vx)+(V1.vy*V2.vy)+(V1.vz*V2.vz)
 
-cross(V1::Vec, V2::Vec) = Vec( (V1.y * V2.z - V1.z * V2.y), (V1.z * V2.x - V1.x * V2.z), (V1.x * V2.y - V1.y * V2.x) )
+# Operations:
 
-# Norm and versor
+cross(V1::Vec, V2::Vec) = Vec( (V1.vy * V2.vz - V1.vz * V2.vy), (V1.vz * V2.vx - V1.vx * V2.vz), (V1.vx * V2.vy - V1.vy * V2.vx) )
+
 squared_norm(V1::Vec) = V1*V1
 
 norm(V1::Vec) = sqrt(squared_norm(V1))
 
-normalize(V1::Vec) = Vec( (V1.x/norm(V1)), (V1.y/norm(V1)), (V1.z/norm(V1)) )
+normalize(V1::Vec) = Vec( (V1.vx/norm(V1)), (V1.vy/norm(V1)), (V1.vz/norm(V1)) )
 
-
-
-# Basic operations for Point
+# Aprroximation for two Point
 Base.:isapprox(P1::Point, P2::Point) = Base.isapprox(P1.x,P2.x) && Base.isapprox(P1.y,P2.y) && Base.isapprox(P1.z,P2.z) 
 
-Base.:+(P1::Point, P2::Point) = Point(P1.x + P2.x, P1.y + P2.y, P1.z + P2.z)
-
-Base.:*(P::Point, a) = Point(P.x*a, P.y*a, P.z*a)
-
 # Sum Point + Vector returning a Point
-Base.:+(P::Point, V::Vec) = Point(P.x + V.x, P.y + V.y, P.z + V.z)
+Base.:+(P::Point, V::Vec) = Point(P.x + V.Vx, P.y + V.Vy, P.z + V.Vz)
+
+# Sum Point + Point returning a Point
+Base.:+(P1::Point, P2::Point) = Point(P1.x + P2.x, P1.y + P2.y, P1.z + P2.z)
 
 # Difference Point - Point returning a Vector
 Base.:-(P1::Point, P2::Point) = Vec(P1.x - P2.x, P1.y - P2.y, P1.z - P2.z)
 
 # Difference Point - Vector returning a Point
-Base.:-(P::Point, V::Vec) = Point(P.x - V.x, P.y - V.y, P.z - V.z)
+Base.:-(P::Point, V::Vec) = Point(P.x - V.Vx, P.y - V.Vy, P.z - V.Vz)
+
+# Product Point * scalar
+Base.:*(P::Point, a) = Point(P.x*a, P.y*a, P.z*a)
