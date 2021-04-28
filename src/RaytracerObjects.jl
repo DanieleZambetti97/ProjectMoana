c39cf219af8df851cfd78e34a97268b1e05a0332
Giacomo Rivolta <giacomo.rivolta@studenti.unimi.it>
2021-04-28 12:31:51 +0200

    Adding ImageTracer struct and its methods

--- src/RaytracerObjects.jl
+++ src/RaytracerObjects.jl
@@ -1,10 +1,10 @@
-import ProjectMoana: Point, Vec
+import ProjectMoana: Point, Vec, HdrImage
 import Base.:≈
 
-export Ray, at
+export Ray, at, fire_all_rays, fire_ray
 
 """
-Denni <3 ...
+This Struct creates a **Ray**. Its constructor takes a origin point (Point type) and a direction vector (Vec type).
 """
 struct Ray
     origin::Point 
@@ -21,4 +21,31 @@ at(ray::Ray, t::Float64) = ray.origin + ray.dir*t
 
 # approx method for testing Ray
 Base.:isapprox(ray1::Ray, ray2::Ray) = Base.isapprox(ray1.origin, ray2.origin) && Base.isapprox(ray1.dir, ray2.dir)
+
+"""
+This struct create a **ImageTracer**. Its constructor takes a raster image (HdrImage) and a camera.
+"""
+struct ImageTracer
+    image::HdrImage
+    camera::Camera
+
+    ImageTracer(image, camera) = new(image, camera)
+end
+
+function fire_ray(im::ImageTracer, col, row, u_pixel::Float64=0.5, v_pixel::Float64=0.5) 
+    u = (col + u_pixel)/(im.image.width - 1)
+    v = (col + v_pixel)/(im.image.height - 1)
+    return fire_ray(im.camera, u, v)
+end
+
+function fire_all_rays(im::ImageTracer, func)
+    for row ∈ 1:im.image.height
+        for col ∈ 1:im.image.width
+            ray = fire_ray(im, col, row)
+            color = func(ray)
+            im.image.set_pixel(col, row, color)
+        end
+    end
+end
+
 
--- test/test_RaytracerObjects.jl
+++ test/test_RaytracerObjects.jl
@@ -1,10 +1,10 @@
 using ProjectMoana
+using ColorTypes
 ## TESTING RAY METHODS ###############################################
 
 ray1 = Ray(Point(1.0, 2.0, 3.0), Vec(5.0, 4.0, -1.0))
 ray2 = Ray(Point(1.0, 2.0, 3.0), Vec(5.0, 4.0, -1.0))
 ray3 = Ray(Point(5.0, 1.0, 4.0), Vec(3.0, 9.0, 4.0))
-
 ray4 = Ray(Point(1.0, 2.0, 4.0), Vec(4.0, 2.0, 1.0))
 
 @testset "Ray tests" begin
@@ -13,4 +13,23 @@ ray4 = Ray(Point(1.0, 2.0, 4.0), Vec(4.0, 2.0, 1.0))
     @test isapprox(at(ray4, 0.0), ray4.origin)
     @test isapprox(at(ray4, 1.0), Point(5.0, 4.0, 5.0))
     @test isapprox(at(ray4, 2.0), Point(9.0, 6.0, 6.0))
+end
+
+## TESTING ImageTracer METHODS ###############################################
+
+image = HdrImage(width = 4, height = 2)
+camera = PerspectiveCamera(aspect_ratio = 2)
+tracer = ImageTracer(image = image, camera = camera)
+
+ray1 = fire_ray(tracer, 0, 0, 2.5, 1.5)
+ray2 = fire_ray(tracer, 2, 1, 0.5, 0.5)
+
+fire_all_rays(tracer, ray -> Color(1.0, 2.0, 3.0))
+
+@testset "ImageTracer tests" begin
+    @test isapprox(ray1, ray2)
+    for row ∈ 1:image.height
+        for col ∈ 1:image.width
+            @test image.get_pixel(col, row) == Color(1.0, 2.0, 3.0)
+    
 end
