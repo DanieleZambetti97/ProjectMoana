288f6e60687fd22434a2ac2cf088720ee2c8eec4
Giacomo Rivolta <giacomo.rivolta@studenti.unimi.it>
2021-05-07 12:57:14 +0200

    Adding World struct and its methods

--- src/Shape.jl
+++ src/Shape.jl
@@ -23,5 +23,30 @@ Base.:≈(H1::HitRecord,H2::HitRecord) = H1.world_point≈H2.world_point && H1.n
 function ray_intersection(sphere::Sphere, ray::Ray)
 end
 
+# Defining the sturct World 
+
+struct Wolrd
+    shapes::Array{Shape}
+    Wolrd() = new([]) 
+end
+
+# Adding shapes method
+add_shape(world::Wolrd, shape::Shape) = append!(world.shapes, shape)
+
+# Overloading for ray_intersection with the struct Wolrd
+function ray_intersection(world::Wolrd, ray::Ray)
+    closest = nothing
+
+    for i ∈ 1:length(world.shapes)
+        intersection = ray_intersection(world.shapes[i], ray)
+        if intersection = nothing
+            continue
+        elseif intersection.t <  
+    end
+
+end
+
+
+
 #########
 
