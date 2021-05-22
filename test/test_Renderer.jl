3669b27d625d41920860702f7ebbc15ce2d31dd0
Giacomo Rivolta <giacomo.rivolta@studenti.unimi.it>
2021-05-22 18:52:52 +0200

    Commit before merge

--- test/test_Renderer.jl
+++ test/test_Renderer.jl
@@ -1,8 +1,8 @@
 using ProjectMoana
 using ColorTypes
 
-sphere = Sphere(translation(Vec(2, 0, 0)) * scaling(Vec(1, 1, 1)), Material(DiffuseBRDF(UniformPigment(RGB(1.,1.,1.)))) )
-image = HdrImage(3, 3)
+sphere = Sphere(translation(Vec(2, 0, 0)) * scaling(Vec(.1, .1, .1)), Material(DiffuseBRDF(UniformPigment(RGB(1.,1.,1.)))) )
+image = HdrImage(9, 9)
 camera = OrthogonalCamera()
 tracer = ImageTracer(image, camera)
 world = World()
@@ -12,10 +12,14 @@ add_shape(world, sphere)
 fire_all_rays(tracer, OnOff_renderer, world)
 
 @testset "Renderer: Test OnOff renderer" begin
-    for i ∈ 1:3
-        println("Riga #$(i)")
-        for j ∈ 1:3
-            println(image.pixels[get_pixel(image, i, j)])
+    for i ∈ 1:9
+        println("\n Riga #$(i)")
+        for j ∈ 1:9
+            if image.pixels[get_pixel(image, i, j)] == RGB(1.,1.,1.)
+                print("W ")
+            elseif image.pixels[get_pixel(image, i, j)] == RGB(0.,0.,0.) 
+                print("B ")
+            end
         end
     end
             
@@ -24,7 +28,7 @@ fire_all_rays(tracer, OnOff_renderer, world)
     @test image.pixels[get_pixel(image,3, 1)]≈RGB(0.,0.,0.)
 
     @test image.pixels[get_pixel(image,1, 2)]≈RGB(0.,0.,0.)
-    @test image.pixels[get_pixel(image,2, 2)]≈RGB(1.,1.,1.) ##
+    @test image.pixels[get_pixel(image,5, 5)]≈RGB(1.,1.,1.) ##
     @test image.pixels[get_pixel(image,3, 2)]≈RGB(0.,0.,0.)
 
     @test image.pixels[get_pixel(image,1, 3)]≈RGB(0.,0.,0.)
